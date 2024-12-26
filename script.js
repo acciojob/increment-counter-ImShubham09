8a4bb2631b77edddc50bb710e92924cabd8bc302
ImShubham09 <singhashubham612@gmail.com>
2024-12-26 19:03:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 document.getElementById("incrementBtn").onclick = function () {
     const counterElement = document.getElementById("counter");
-    alert(`Current value: ${counterElement.textContent}`);
+    alert(counterElement.textContent);
     counterElement.textContent = parseInt(counterElement.textContent, 10) + 1;
 };
